8260e0bd2a4e1b84e66f297874ab12aa0da8c047
Olha Kravchenko <olga19972010@yandex.by>
2021-05-27 18:17:15 +0300

    7.1 Added handlers for changing filters.

--- js/gallery.js
+++ js/gallery.js
@@ -7,7 +7,11 @@
   const closeButton = preview.querySelector(`#picture-cancel`);
   const commentCounter = preview.querySelector(`.social__comment-count`);
   const commentLoader = preview.querySelector(`.comments-loader`);
+  const filterDefault = document.querySelector(`#filter-default`);
+  const filterRandom = document.querySelector(`#filter-random`);
+  const filterDiscussed = document.querySelector(`#filter-discussed`);
   let photos;
+  let newArrPhotos = [];
 
   const addId = (photosToRender) => {
     for (let i = 0; i < photosToRender.length; i++) {
@@ -20,16 +24,84 @@
     for (let i = 0; i < photosToRender.length; i++) {
       let photoToRender = photosToRender[i];
       const newPhoto = window.picture.render(photoToRender);
-      photos = photosToRender;
       fragment.appendChild(newPhoto);
     }
   };
 
-  const render = (photosToRender) => {
+  const renderRandomPhotos = (photosToRender, fragment) => {
+    let photoShown = [];
+    while (photoShown.length < 10) {
+      const randomNumber = window.util.getRandomNumber(0, photosToRender.length);
+      let photoToRender = photosToRender[randomNumber];
+      if (!photoShown.includes(photoToRender)) {
+        photoShown.push(photoToRender);
+        const newPhoto = window.picture.render(photoToRender);
+        fragment.appendChild(newPhoto);
+      }
+    }
+  };
+
+  const renderSortPhotos = (photosToRender, fragment) => {
+    let photoSortShown = [...photosToRender];
+    for (let i = 0; i < photoSortShown.length; i++) {
+      photoSortShown.sort((o1, o2) => o2.comments.length - o1.comments.length);
+      let photoToRender = photoSortShown[i];
+      const newPhoto = window.picture.render(photoToRender);
+      fragment.appendChild(newPhoto);
+    }
+  };
+
+  const render = (photosToRender, bottom) => {
     const fragment = document.createDocumentFragment();
-    renderPhotos(photosToRender, fragment);
+    if (bottom === filterRandom) {
+      renderRandomPhotos(photosToRender, fragment);
+    } else if (bottom === filterDefault) {
+      renderPhotos(photosToRender, fragment);
+    } else if (bottom === filterDiscussed) {
+      renderSortPhotos(photosToRender, fragment);
+    } else {
+      renderPhotos(photosToRender, fragment);
+    }
     pictures.appendChild(fragment);
   };
+
+  const updatePhotos = (bottom) => {
+    render(newArrPhotos, bottom);
+  };
+
+  const renderUpdatePhotos = (data) => {
+    newArrPhotos = data;
+    window.myCoolPhotos = data;
+    updatePhotos();
+  };
+
+  const removePictures = () => {
+    document.querySelectorAll(`.picture`).forEach((e)=>e.remove());
+  };
+
+  filterDefault.addEventListener(`click`, () => {
+    removePictures();
+    filterDefault.classList.add(`img-filters__button--active`);
+    filterRandom.classList.remove(`img-filters__button--active`);
+    filterDiscussed.classList.remove(`img-filters__button--active`);
+    updatePhotos(filterDefault);
+  });
+
+  filterRandom.addEventListener(`click`, () => {
+    removePictures();
+    filterRandom.classList.add(`img-filters__button--active`);
+    filterDefault.classList.remove(`img-filters__button--active`);
+    filterDiscussed.classList.remove(`img-filters__button--active`);
+    updatePhotos(filterRandom);
+  });
+
+  filterDiscussed.addEventListener(`click`, () => {
+    removePictures();
+    filterDiscussed.classList.add(`img-filters__button--active`);
+    filterDefault.classList.remove(`img-filters__button--active`);
+    filterRandom.classList.remove(`img-filters__button--active`);
+    updatePhotos(filterDiscussed);
+  });
 
   const showModal = () => {
     preview.classList.remove(`hidden`);
@@ -67,10 +139,16 @@
     document.addEventListener(`keydown`, onEscKeydown);
   };
 
+  const imgFilters = document.querySelector(`.img-filters`);
+  const showFilters = () => {
+    imgFilters.classList.remove(`img-filters--inactive`);
+  };
+
   const activate = (photosNew) => {
     addId(photosNew);
-    render(photosNew);
+    renderUpdatePhotos(photosNew);
     addListeners();
+    showFilters();
   };
 
   window.gallery = {
