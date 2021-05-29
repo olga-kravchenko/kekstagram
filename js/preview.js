0cf34ee6a479ef70d5fc3d32af0cab857af30272
Olha Kravchenko <olga19972010@yandex.by>
2021-05-29 08:33:30 +0300

    7.2 Updated the list of comments.

--- js/preview.js
+++ js/preview.js
@@ -1,8 +1,14 @@
 'use strict';
 
 (() => {
+  const QUANTITY_SHOW_COMMENTS_STEP = 5;
+
   const preview = document.querySelector(`.big-picture`);
   const socialComments = preview.querySelector(`.social__comments`);
+  const commentCounter = preview.querySelector(`.social__comment-count`);
+  const commentLoader = preview.querySelector(`.comments-loader`);
+
+  let shownCommentsQuantity = 0;
 
   const createNewComment = (comment) => {
     const newComment = window.util.createNewElement(`li`, `social__comment`);
@@ -15,11 +21,30 @@
     return newComment;
   };
 
-  const addCommentsToParent = (parent, comments) => {
-    window.util.cleanContent(socialComments);
-    comments.forEach((comment) => {
-      parent.appendChild(createNewComment(comment));
-    });
+  const hideCommentLoader = () => {
+    commentLoader.classList.add(`hidden`);
+  };
+
+  const showCounterAndCommentLoader = () => {
+    commentCounter.classList.remove(`hidden`);
+    commentLoader.classList.remove(`hidden`);
+  };
+
+  const addCommentsToParent = (comments) => {
+    let start = shownCommentsQuantity;
+    if (shownCommentsQuantity + QUANTITY_SHOW_COMMENTS_STEP < comments.length) {
+      shownCommentsQuantity += QUANTITY_SHOW_COMMENTS_STEP;
+      showCounterAndCommentLoader();
+    } else {
+      shownCommentsQuantity = comments.length;
+      hideCommentLoader();
+    }
+
+    preview.querySelector(`#comments-counter`).textContent = shownCommentsQuantity;
+
+    for (start; start < shownCommentsQuantity; start++) {
+      socialComments.appendChild(createNewComment(comments[start]));
+    }
   };
 
   const fillPreviewByInfo = (photo) => {
@@ -30,11 +55,16 @@
   };
 
   const render = (photo) => {
+    shownCommentsQuantity = 0;
+    window.util.cleanContent(socialComments);
+    commentLoader.addEventListener(`click`, () => {
+      addCommentsToParent(photo.comments);
+    });
     fillPreviewByInfo(photo);
-    addCommentsToParent(socialComments, photo.comments);
+    addCommentsToParent(photo.comments);
   };
 
   window.preview = {
-    render
+    render,
   };
 })();
